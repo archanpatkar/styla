faf9caaddf7795e9abe8dde18e32b74dec058077
archanpatkar <pat@archanpatkar.com>
2020-07-31 20:55:23 +0530

    cli config

--- src/index.js
+++ src/index.js
@@ -1,45 +1,51 @@
+#!/usr/bin/env node
 const readline = require('readline');
-const tokenize = require("./lexer");
-const parse = require("./parser");
-const typecheck = require("./type");
-const eval = require("./eval");
+const { Interpreter, modes } = require("./eval");
 
+const machine = new Interpreter();
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'styla> '
 });
 
+console.log(
+`
+        __          __        
+.-----.|  |_.--.--.|  |.---.-.
+|__ --||   _|  |  ||  ||  _  |
+|_____||____|___  ||__||___._|
+            |_____|           
+      
+`)
 console.log("styla (Simply Typed Lambda Calculus) 0.0.1");
 console.log("");
 
 rl.prompt();
-let log = false;
 
 rl.on('line', (input) => {
     if(input == "help") {
+        console.log("Type mode <m> where m in [value, name, need] to change evaluation method")
         console.log("Type 'clear' to clean the console")
         console.log("Type 'exit' or Ctrl + c to exit")
-        console.log("Type 'log' to toggle (on) / (off) logging of Tokens and AST")
     }
-    else if(input == "log") log = !log;
     else if(input == "clear") console.clear();
     else if(input == "exit") {
         rl.close();
         return;
     }
+    else if(input.startsWith("mode")) {
+        let mode = modes[input.split(" ")[1]];
+        if(mode) machine.setMode(mode)
+        else console.log("Please specify a proper mode");
+    }
     else {
         try {
-            const tokens = tokenize(input);
-            if(log) console.log(tokens);
-            const ast = parse(tokens);
-            if(log) console.log(JSON.stringify(ast,undefined,4));
-            const type = typecheck(ast)
-            const out = eval(ast);
-            console.log(`${type}: ${out}`);
+            const {output, type} = machine.evaluate(input);
+            console.log(`${type}: ${output}`);
         }
         catch (e) {
-            console.log(e.message)
+            console.log(`Error: ${e.message}`)
         }
     }
     rl.prompt();
